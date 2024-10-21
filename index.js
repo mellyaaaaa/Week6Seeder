17f824d04a80792203a28ecf02406ddae14d6d1d
ASUS <mellyanggraini2801@gmail.com>
2024-10-21 15:42:54 +0700

    initial commit for seeder project

---
+++ index.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+const fs = require("fs");
+
+require("dotenv").config();
+
+async function main() {
+  /**--------------- Not allowed to be edited - start - --------------------- */
+  const mongoUri = process.env.MONGODB_URI;
+  const collection = process.env.MONGODB_COLLECTION;
+
+  const args = process.argv.slice(2);
+
+  const command = args[0];
+  /**--------------- Not allowed to be edited - end - --------------------- */
+
+  // Connect to MongoDB
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  // Define a schema for the collection
+  const schema = new mongoose.Schema({}, { strict: false });
+  const MovieModel = mongoose.model(collection, schema);
+
+  switch (command) {
+    case "check-db-connection":
+      await checkConnection();
+      break;
+    case "reset-db":
+      await MovieModel.deleteMany();
+      break;
+    case "bulk-insert":
+      const data = fs.readFileSync("./seed.json");
+      const parsed = JSON.parse(data);
+      await MovieModel.insertMany(parsed);
+      break;
+      case "get-all":
+        try {
+          const allMovies = await MovieModel.find();
+          console.log("All movies:", allMovies);
+        } catch (error) {
+          console.error("Error fetching all data:", error);
+        }
+        break;
+      
+    // TODO: Buat logic fungsionalitas yg belum tersedia di bawah
+    default:
+      throw Error("command not found");
+  }
+
+  await mongoose.disconnect();
+  return;
+}
+
+async function checkConnection() {
+  console.log("check db connection started...");
+  try {
+    await mongoose.connection.db.admin().ping();
+    console.log("MongoDB connection is successful!");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err);
+  }
+  console.log("check db connection ended...");
+}
+
+main();
